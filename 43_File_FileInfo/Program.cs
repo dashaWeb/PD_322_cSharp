c8ee6f02dff14ebc23756fee29e8be929c13e446
Dasha <konopelko@step.rivne>
2024-01-29 16:18:06 +0200

    File and Directory

---
+++ 43_File_FileInfo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_File_FileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "First line", "Second Line" };
+            string fname = "my.txt";
+
+            File.SetAttributes(fname, FileAttributes.Archive);
+
+            if (File.Exists(fname))
+            {
+                File.Delete(fname);
+                Console.WriteLine($"File '{fname}' was deleted\n\n");
+            }
+            File.AppendAllLines(fname, lines);
+            File.AppendAllText(fname, "Third line\nFourth line");
+
+            Console.WriteLine($"Content of file \n\n{File.ReadAllText(fname)}");
+
+            string fname2 = "../../copy_my.txt";
+            //File.Copy(fname, fname2);// exception якщо файл вже існує fname2
+            File.Copy(fname, fname2,true); // дозвіл на перезапис у файл копію fname2
+            Console.WriteLine($"\nContent of copy file '{fname2}' \n{File.ReadAllText(fname2)}");
+
+
+            /*var fs = File.Create(fname);
+            fs.WriteByte(65);*/
+            //var fs = File.AppendText(fname);
+            var fs = File.CreateText(fname);
+            fs.WriteLine("Write something .... 2");
+            fs.Close();
+
+            File.AppendAllText(fname, "New line");
+            Console.WriteLine($"Content of file (after append) '{fname}' \n{File.ReadAllText(fname)}");
+
+            Console.WriteLine($"Creation time {File.GetCreationTime(fname)}");
+            Console.WriteLine($"Last write time {File.GetLastWriteTime(fname)}");
+
+            string fname3 = "../../third.txt";
+            FileInfo fi = new FileInfo(fname3);
+
+            Console.WriteLine($"\n\n Is exists file '{fname3}' :: {fi.Exists}");
+            if(!fi.Exists)
+            {
+                using(var tw = fi.CreateText())
+                {
+                    tw.WriteLine("It is third file");
+                }
+            }
+
+            Console.WriteLine($"Content of file '{fname3}' \n{File.ReadAllText(fname3)}");
+
+            //fi.MoveTo("./third.txt"); // поточна папка, зараз це папка .../bin/debug
+            //Console.WriteLine($"Move to path : {Path.Combine(".",fname3)}"); // bad example
+            //Console.WriteLine($"Move to path : {Path.Combine(".",fi.Name)}"); 
+            //fi.MoveTo(Path.Combine(".", fi.Name)); // поточна папка, зараз це папка .../bin/debug
+
+            Console.WriteLine($"Length of '{fname3}' :: {fi.Length}");
+            Console.WriteLine($"Extension of '{fname3}' :: {fi.Extension}");
+            Console.WriteLine($"File attributes of '{fname3}' :: {File.GetAttributes(fname3)}");
+
+            File.SetAttributes(fname3, FileAttributes.ReadOnly);
+            Console.WriteLine($"File attributes of '{fname3}' :: {File.GetAttributes(fname3)}");
+        }
+    }
+}
---
+++ 44_Directory_DirectoruInfo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_Directory_DirectoruInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Current path : \n{Directory.GetCurrentDirectory()}");
+            Directory.CreateDirectory("A");
+
+            Directory.SetCurrentDirectory("A"); // поточний шлях
+            Console.WriteLine($"Current path : \n{Directory.GetCurrentDirectory()}");
+
+            Directory.CreateDirectory("A1");
+            Directory.CreateDirectory("A2");
+
+            File.WriteAllText("a.txt", "File a.txt content");
+            File.WriteAllText("b.txt", "File b.txt content");
+
+            File.WriteAllText("A1/a1.txt", "File a1.txt content");
+            File.WriteAllText("A2/a2.txt", "File a2.txt content");
+
+            // get files from folder A
+            //string[] fnames = Directory.GetFiles(".","a*.*"); // 
+            string[] fnames = Directory.GetFiles(".","a*.*",SearchOption.AllDirectories); // 
+            foreach (var item in fnames)
+            {
+                Console.WriteLine($"{Path.GetFileName(item)} in folder {Path.GetDirectoryName(item)}");
+            }
+
+            string[] dirs = Directory.GetDirectories(".");
+            Console.WriteLine($"\n\n List folders from folder A");
+            foreach (var item in dirs)
+            {
+                Console.WriteLine(Path.GetFileName(item));
+            }
+
+
+            string path = @"C:\Users\konopelko\Downloads";
+            string[] entries = Directory.GetFileSystemEntries(path);
+
+            Console.OutputEncoding= Encoding.UTF8;
+            Console.WriteLine($"\n\n----- {path}");
+            foreach (var item in entries)
+            {
+                FileInfo fi = new FileInfo(item);
+                string info = "<DIR>";
+                if(!fi.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    info = fi.Length.ToString();
+                }
+                Console.WriteLine($"{fi.CreationTime,-22} {fi.Name,-50} {info,-15}");
+            }
+
+            path = "B";
+            DirectoryInfo di = new DirectoryInfo(path);
+            if(!di.Exists)
+            {
+                di.Create();
+            }
+            Console.WriteLine($"Attributes of B : {di.Attributes}");
+            File.WriteAllText("B/b1.txt", "Hello from b1.txt");
+            File.WriteAllText("B/b2.txt", "Hello from b2.txt !!! ");
+            FileInfo[] list = di.GetFiles();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Name} :: {item.Length}");
+            }
+        }
+    }
+}
